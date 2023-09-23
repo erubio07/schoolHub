466962225ee3166d270e118aed37983612c7e495
Ezequiel Rubio <erubio07@gmail.com>
2023-09-23 10:54:31 -0300

    componentes principales creados

--- client/schoolhub/src/App.js
+++ client/schoolhub/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Views/Home";
+import About from "./Views/About";
+import ContacUs from "./Views/ContacUs";
+import Login from "./Views/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<ContacUs />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
